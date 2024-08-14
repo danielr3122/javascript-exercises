84ba9cd36758bd5e78170bbb2d09124d3f90b4d7
danielr3122 <drosenhamer@gmail.com>
2024-08-14 14:37:41 -0500

    feat: finished exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeUs) {
+    result = []
+    addMe = false
+    for(let i = 0; i < array.length; i++) {
+        addMe = true;
+        for(let removeMe of removeUs) {
+            if (array[i] === removeMe) {
+                addMe = false;
+            }
+        }
+        if (addMe) result.push(array[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp -= 32;
+  temp *= (5 / 9);
+  return Math.round(10 * temp) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp *= (9 / 5);
+  temp += 32;
+  return Math.round(10 * temp) / 10;
 };
 
 // Do not edit below this line
